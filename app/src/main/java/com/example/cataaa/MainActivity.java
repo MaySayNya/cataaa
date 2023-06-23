957359ef6b946247e629f3da00ad9fae3824acf5
Marceline Rozelaar <marceline.rozelaar@gmail.com>
2023-06-23 21:07:49 +0200

    added button functionality

--- app/src/main/java/com/example/cataaa/MainActivity.java
+++ app/src/main/java/com/example/cataaa/MainActivity.java
@@ -7,6 +7,8 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -16,13 +18,15 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String IMAGEURL = "https://cataas.com/cat";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ImageView iv = findViewById(R.id.imageView);
+
+        Button b = findViewById(R.id.button);
 
         // Declaring executor to parse the URL
         Executor executor = Executors.newSingleThreadExecutor();
@@ -31,29 +35,23 @@ public class MainActivity extends AppCompatActivity {
         // in the ImageView
         Handler h = new Handler(Looper.getMainLooper());
 
-        // Background process for network operation
-        executor.execute(() -> {
-            // Image URL
-            String imageURL = "https://cataas.com/cat";
+        b.setOnClickListener(view -> {
+            // Background process for network operation
+            executor.execute(() -> {
+                // Try to get the image and post it in the ImageView
+                try {
+                    InputStream is = (new URL(IMAGEURL)).openStream();
+                    Bitmap image = BitmapFactory.decodeStream(is);
+                    // Leave background thread to make ui change
+                    h.post(() -> iv.setImageBitmap(image));
+                }
 
-            // Try to get the image and post it in the ImageView
-            try {
-                InputStream is = (new URL(imageURL)).openStream();
-                Bitmap image = BitmapFactory.decodeStream(is);
-
-                Looper.prepare();
-                Toast.makeText(getApplicationContext(), "Loaded cat!", Toast.LENGTH_LONG).show();
-
-                // Leave background thread to make ui change
-                h.post(() -> iv.setImageBitmap(image));
-            }
-
-            catch (Exception e) {
-                e.printStackTrace();
-                Looper.prepare();
-                Toast.makeText(getApplicationContext(), "Could not load cat!", Toast.LENGTH_LONG).show();
-            }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
         });
+
     }
 
 }
